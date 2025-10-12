fa5b3f4664f85b85ad9b47c0e394b49a2e0f5d7f
Paige Li <itspaige.nli@gmail.com>
2025-10-12 11:41:25 -0700

    Exercise 8 Complete

--- prework/8_ObjectsChallenges.js
+++ prework/8_ObjectsChallenges.js
@@ -19,6 +19,18 @@
 // let obj3 = {};
 // keys(obj3); // []
 
+function keys(obj1) {
+    let arrKey = [];
+    console.log("before loop")
+    for (const key in obj1) {
+        console.log("inside loop: "+ key)
+        arrKey.push(key);
+    }
+    return arrKey;
+}
+
+
+
 // Exercise 2. Write a function called values, which accepts an object and returns an array of all of the values in the object.
 // IMPORTANT: Do not use the built in Object.values() function!
 // Examples:
@@ -29,8 +41,19 @@
 // let obj3 = {};
 // values(obj3); // []
 
+function values(obj1) {
+    let arrVal = [];
+    console.log("before loop")
+    for (const key in obj1) {
+        console.log("inside loop: "+ obj1[key])
+        arrVal.push(obj1[key]);
+    }
+    return arrVal;
+}
+
 // Exercise 3. Write a function called entries, which accepts an object and returns an array of arrays of key-value pairs.
-// In other words, each sub-array is an "entry" in the object with two elements: the first element is the key, and the second element is the value.
+// In other words, each sub-array is an "entry" in the object with two elements: the first element is the key, and the second element
+//  is the value.
 // IMPORTANT: Do not use the built in Object.entries() function!
 // Examples:
 // let obj = { a: 1, b: 2, c: 3 };
@@ -42,13 +65,32 @@
 // let obj3 = {};
 // entries(obj3); // []
 
+function entries(obj1) {
+    let arrEntries = [];
+    console.log("before loop")
+    for (const key in obj1) {
+        console.log("inside loop: "+ obj1[key])
+        arrEntries.push([key,obj1[key]]);
+    }
+    return arrEntries;
+}
+
 // Exercise 4. Write a function called pluck, which takes an array of objects and the name of a key.
-// The function should return an array containing the value associated with that key for each object, or undefined if that key is not present in the object.
+// The function should return an array containing the value associated with that key for each object, or undefined if that key is 
+// not present in the object.
 // Examples:
 // pluck([{ name: "Tim" }, { name: "Matt" }, { name: "Elie" }],'name')
 // ["Tim", "Matt", "Elie"]
 // pluck([{ name: "Tim", isBoatOwner: true }, { name: "Matt", isBoatOwner: false }, { name: "Elie" }],'isBoatOwner')
 // [true, false, undefined]
+
+function pluck(arr1, keyName){
+    let arrValue = [];
+    for (let i=0; i<arr1.length;i++) {
+        arrValue.push(arr1[i][keyName]);
+    }
+    return arrValue;
+}
 
 // Exercise 5. Write a function called stringFromObject that generates a string from an object's key/value pairs.
 // The format should be "key = value, key = value".
@@ -60,6 +102,34 @@
 // "name = Elie, job = Instructor, isCatOwner = false"
 // stringFromObject({}); // ""
 
+function stringFromObjectForeach(obj1) {
+    let stringPairs = "";
+    console.log("before loop")
+    for (const key in obj1) {
+        console.log("inside loop: "+ obj1[key])
+        stringPairs = stringPairs + key + " = " + obj1[key] + ", ";
+    }
+    if (stringPairs.length>0)
+        return stringPairs.substring(0,stringPairs.length-2);
+    else
+        return stringPairs;
+}
+
+function stringFromObject(obj1) {
+    let stringPairs = "";
+    const keys = Object.keys(obj1);
+
+    console.log("before loop")
+    for (let i=0;i< keys.length;i++) {
+        console.log("inside loop: "+ keys[i])
+        stringPairs = stringPairs + keys[i] + " = " + obj1[keys[i]];
+        if (i < keys.length-1)
+        {
+            stringPairs = stringPairs + ", "
+        }
+    }
+    return stringPairs;
+}
 // Exercise 6. Write a function called minMaxKeyInObject that accepts an object with numeric keys.*
 // The function should return an array with the following format: [lowestKey, highestKey]
 // Examples:
@@ -67,3 +137,22 @@
 // [1, 10]
 // minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' });
 // [1, 4]
+
+function minMaxKeyInObject(obj1) {
+    const keys = Object.keys(obj1);
+
+    let lowestKey = parseInt(keys[0]);
+    let highestKey = parseInt(keys[0]);
+    
+
+    console.log("before loop")
+    for (let i=0;i< keys.length;i++) {
+        let intKey = parseInt(keys[i]);
+        console.log("inside loop: " + keys[i]);
+        if (intKey < lowestKey)
+            lowestKey = intKey;
+        if (intKey > highestKey)
+            highestKey = intKey;
+    }
+    return [lowestKey, highestKey];
+}
