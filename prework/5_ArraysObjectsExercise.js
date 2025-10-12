970329c2d2ae6495d7e5077f1014b9bbde905059
Paige Li <itspaige.nli@gmail.com>
2025-10-12 08:20:44 -0700

    Exercise 5 Completed

--- prework/5_ArraysObjectsExercise.js
+++ prework/5_ArraysObjectsExercise.js
@@ -10,33 +10,47 @@
 // But make sure you understand why the correct answer is right.
 
 // Exercise 1. Make an empty array named animals
-animals[]
+let animals = [];
 
 // Exercise 2. Add the string "frog" to the array
 animals.push("frogs");
 
 // Exercise 3. Add 4 more animals of your choice to the array. Try to add all of them in one line of code.
-animals.push("elephants");
-animals.push("bear");
-animals.push("foxes");
+animals.push("elephants", "bear", "foxes");
 
 // Exercise 4. Update the first item in the array to be "gorilla"
 
+animals[0] = "gorilla";
+
 // Exercise 5. Print the number of items in the array (use the array property that will give you this information)
+
+console.log(animals.length);
 
 // Exercise 6. Print the first item in the array
 
+console.log(animals[0]);
+
 // Exercise 7. Print the last item in the array (as part of your answer, use the same array property you used in exercise 5 above)
+
+console.log(animals[animals.length-1]);
 
 // Exercise 8. Remove the last item from the array
 
+animals.pop();
+
 // Exercise 9. Now make a new array named assortedThings that contains at least 3 strings and 3 numbers.
+
+let assortedThings = ["suki", "cricket", "koa", "sasha", 1, 2, 3];
 
 // Exercise 10. Write a for loop that iterates through the assortedThings array and prints each index and item, such as:
 // Item #0 is potato
 // Item #1 is 4
 // Item #2 is Hello World
 // ...
+let x;
+for(x = 0; x <= assortedThings.length-1; x++) {
+   console.log("Item #" + x + " is " + assortedThings[x]); 
+}
 
 // Exercise 11. Write a function that takes an array of numbers as a parameter. For each number in the array,
 // print the number and "BIG" if it's over 100, "small" if it's between 0 and 100, and "negative" if
@@ -48,15 +62,43 @@ animals.push("foxes");
 //
 // Then write some function calls you would use to test your function.
 
+function checkNumbers(arr) {
+for (let index = 0; index <= arr.length-1; index++){
+    if (arr[index] > 100)
+        console.log(arr[index] + " BIG");
+    else if (arr[index] >= 0 && arr[index] <= 100)
+        console.log(arr[index] + " small");
+    else 
+        console.log(arr[index] + " negative");
+}
+}
+checkNumbers([-2, 50]);
+
+
 // Exercise 12. Make an object called me that contains the following keys: name, favoriteAnimal, favoriteNumber
 // The values of those keys can be whatever you choose.
 
+let me = {
+    myName: "Paige",
+    favoriteAnimal: "Cat",
+    favoriteNumber: 3
+}
+
 // Exercise 13. Add another 2 keys and values to the object (you choose what they are)
+
+me.age = 38;
+me.favoriteColor = "forest green";
 
 // Exercise 14. Update the favoriteAnimal value to something different
 
+me.favoriteAnimal = "Cats";
+
 // Exercise 15. Print the value of favoriteAnimal.
 // Note: there are at least 2 ways to get the value of a key, try to write both.
+
+console.log(me.favoriteAnimal);
+
+console.log(me["favoriteAnimal"]);
 
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you?
